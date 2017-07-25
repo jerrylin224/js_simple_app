9da22185677fa81cb3bbbd54e1e3fbbdc4e14a78
Mata Angelou <pk60905@hotmail.com>
2017-07-25 09:54:21 +0800

    Finished basic feature, trying to extract code to prev and next funciton to trigger

--- slide_show/slide.js
+++ slide_show/slide.js
@@ -9,7 +9,55 @@
 // - else if id equals to first index, slide to last
 
 $(function() {
+  var $mySlides = $('.mySlides'),
+      lastIdx = $mySlides.length - 1;
+
+
   $(".slideshow-container").on('click', '.slide', function(e) {
-    console.log(e.target.classList.contains("next"));
+    var currentIdx = +$('.active').attr('id') - 1;
+
+    if (e.target.classList.contains("next"))  {
+        $mySlides.stop().filter(":visible").fadeOut(300);
+        $('.active').removeClass('active');
+      if (currentIdx != lastIdx) {
+        $('.mySlides').eq(currentIdx + 1).addClass('active').delay(300).fadeIn(300);
+      } else {
+        $('.mySlides').eq(0).addClass('active').delay(300).fadeIn(300);
+      }
+    } else if (e.target.classList.contains("prev")) {
+        $mySlides.stop().filter(":visible").fadeOut(300);
+        $('.active').removeClass('active');
+        if (currentIdx != 0) {
+          $('.mySlides').eq(currentIdx - 1).addClass('active').delay(300).fadeIn(300);
+        } else {
+          $('.mySlides').eq(lastIdx).addClass('active').delay(300).fadeIn(300);
+        }
+    }
+
   })
 });
+
+// 確認一下contain的用法
+// 我這樣用不會太麻煩嗎？
+
+// 參考w3c slide
+
+
+      // $(function() {
+      //   var $slideshow = $("#slideshow"),
+      //       $nav = $slideshow.find("ul");
+
+
+      //   $("main ul").on('click', 'a', function(e) {
+      //     e.preventDefault();
+      //     var $li = $(e.currentTarget).closest('li');
+      //     var idx = $li.index();
+
+      //     $mySlides.find("figure").stop().filter(":visible").fadeOut(300);
+      //     $mySlides.find("figure").eq(idx).delay(300).fadeIn(300);
+      //     $nav.find(".active").removeClass("active");
+      //     $li.addClass("active");
+      //   });
+      // });
+
+// 7.24問題：無法有效的產生fade-in/fade-out的效果
