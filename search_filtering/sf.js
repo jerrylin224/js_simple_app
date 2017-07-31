f2ec065b1cd4048068e05f629a3597d14ce702a5
Mata Angelou <pk60905@hotmail.com>
2017-07-31 10:54:34 +0800

    Select checked categories and iterate through the items to show or hide

--- search_filtering/sf.js
+++ search_filtering/sf.js
@@ -1,6 +1,11 @@
 $(function() {
-
-})
+  // var $checkedItems = $("form input[type='checkbox']:checked").parents("label")
+  // var showItems = $checkedItems.map(function(idx, item) {
+  //   return item.textContent.trim();
+  // });
+  // showItems.each(function(idx, item) {
+  //   $("main li:contains(" + item + ")").hide();
+  // });})
 
 // click each tag, only those option fit condition show
 // User a listener to listen to the event
