1e95d2f73c6b7517760e91d1bdd6070178e4447b
Mata Angelou <pk60905@hotmail.com>
2017-08-01 10:04:45 +0800

    finish using checkbox to filter feature.

--- search_filtering/sf.js
+++ search_filtering/sf.js
@@ -1,13 +1,7 @@
 $(function() {
-  // var $checkedItems = $("form input[type='checkbox']:checked").parents("label")
-  // var showItems = $checkedItems.map(function(idx, item) {
-  //   return item.textContent.trim();
-  // });
-  // showItems.each(function(idx, item) {
-  //   $("main li:contains(" + item + ")").hide();
-  // });})
-
-// click each tag, only those option fit condition show
-// User a listener to listen to the event
-// check the text. if certain text contained
-// 
+  $("input[type='checkbox']").change(function(e) {
+    var $this = $(this);
+    var changedItemContent = $this.val();
+    $("main li:contains(" + changedItemContent + ")").toggle();
+  });
+});
